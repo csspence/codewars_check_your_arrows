a590c8401e677c4b8adf180bbb36842a4b72841b
Crew Spence <csspence123@gmail.com>
2021-01-09 08:35:44 -0600

    Wrote working solution.

--- codewars_check_your_arrows.js
+++ codewars_check_your_arrows.js
@@ -9,7 +9,11 @@ If an arrow in the quiver does not have a damaged status, it means it's new.
 The expected result is a boolean, indicating whether you have any good arrows left.
 */
 
-function anyArrows(arrows){
-  // arrow it
-  return arrows.some( => );
+const anyArrows = (arrows) => {
+  for(let i = 0; i < arrows.length; i++) {
+    if(arrows[i].damaged !== true) {
+      return true;
+    }
+  }
+  return false;
 }
